69a35e8ab81cb3d7180039be19a2d0a476efed7d
ironman123 <rajulyadav123@gmail.com>
2024-11-19 16:16:54 +0530

    Updated main.js for Vue 3

--- app/src/main.js
+++ app/src/main.js
@@ -1,13 +1,10 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router';
 import store from './store';
+import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-  router,
-  store
-}).$mount('#app')
+createApp(App)
+.use(store)
+.use(router)
+.mount('#app')
