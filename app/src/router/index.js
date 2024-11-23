7ed4e730dc473d84d0db231b6a90499ae5fa09fc
ironman123 <rajulyadav123@gmail.com>
2024-11-23 12:11:29 +0530

    Fixed Token Check on Redirection

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -10,13 +10,13 @@ const routes = [
   {
     path: '/',
     component: SignIn,
-    beforeEnter:(to,from,next)=>{
-      if (store.state.isAuthenticated) {
-        next('/dashboard');
-      } else{
-        next();
-      }
-    },
+    // beforeEnter:(to,from,next)=>{
+    //   if (store.state.isAuthenticated) {
+    //     next('/dashboard');
+    //   } else{
+    //     next();
+    //   }
+    // },
   },
   {
     path: '/dashboard',
@@ -35,14 +35,49 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
-  if(to.meta.requiresAuth && !store.state.isAuthenticated)
-  {
-    next('/');
+async function validateToken() {
+  const token = localStorage.getItem("token");
+  if (!token) return false;
+  console.log(token);
+  try {
+    const response = await fetch("http://127.0.0.1:5000/auth/check-token", {
+      method: "GET",
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log("Token validation success:", data);
+      return true;
+    } else {
+      console.warn("Token validation failed.");
+      return false;
+    }
+  } catch (error) {
+    console.error("Error validating token:", error);
+    return false;
   }
-  else if (to.path === "/" && store.state.isAuthenticated) {
-    next("/dashboard");
-  } 
+}
+
+router.beforeEach(async(to, from, next) => {
+  const isAuthenticated = await validateToken();
+  console.log(isAuthenticated);
+  if (to.meta.requiresAuth) 
+  {    
+    if (!isAuthenticated) {
+      store.dispatch('signOut')
+      next("/"); // Redirect to login
+    }
+    
+  }
+  else if (isAuthenticated && to.path === '/')
+  {
+    next('/dashboard')
+  }
+  //const isAuthenticated = await validateToken();
+  
   next();
 });
 
