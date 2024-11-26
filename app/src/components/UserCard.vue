8331f05b1f62c15251799881048008853f675d3b
ironman123 <rajulyadav123@gmail.com>
2024-11-26 16:09:22 +0530

    Implemented User Page for Admin

---
+++ app/src/components/UserCard.vue
@@ -0,0 +1,197 @@
+<template>
+    <div :class="['user-card card mb-3', { dark: isDarkTheme }]">
+      <div :class="['card-header d-flex justify-content-between align-items-center', { dark: isDarkTheme }]">
+      <!-- Card Title aligned to the start -->
+      <h5 class="card-title mb-0">{{ user.name }}</h5>
+        <div>  
+          <!-- Status Badge aligned to the end with color changes based on the status -->
+            <div>
+                <span style="margin-right: 0.5rem;" :class="[
+                        'badge',
+                        { 'bg-info': user.userType === 'Influencer', 
+                          'bg-primary': user.userType === 'Sponsor' },
+                        { dark: isDarkTheme }
+                    ]"
+                >
+                    {{ user.userType }}
+                </span>
+                <span :class="[
+                        'badge',
+                        { 'bg-danger': user.flag === 'Flagged', 
+                          'bg-warning': user.flag === 'Unauthorized', 
+                          'bg-success': user.flag === 'Authorized' },
+                        { dark: isDarkTheme }
+                    ]"
+                >
+                    {{ user.flag }}
+                </span>
+            </div>
+        </div>
+      </div>
+      <!-- <div :class="['card-body', { dark: isDarkTheme }]">
+        <p class="card-text"><strong>Sponsor:</strong> {{ user.owner }}</p>
+        <strong>Description:</strong>
+        <p class="card-text"> {{ user.description }}</p>
+        <div class="row" style="width: 100%;">
+          <div class="col">
+            <p class="card-text"><strong>Goals:</strong> {{ user.goals }}</p>
+          </div>
+          <div class="col">
+            <p class="card-text"><strong>Budget:</strong> ${{ user.budget }}</p>
+          </div>
+        </div>  
+      </div> -->
+      <div :class="['card-footer d-flex justify-content-between align-items-center', { dark: isDarkTheme }]">
+        <div>
+          <button
+            v-if="isAdmin"
+            class="btn btn-secondary btn-sm"
+            @click="toggleFlag"
+          >
+            Toggle Flag
+          </button>
+          <button
+            v-if="isAdmin && user.userType === 'Sponsor' && user.flag === 'Unauthorized'"
+            class="btn btn-success btn-sm"
+            @click="authorize"
+          >
+            Authorize
+          </button>
+        </div>
+        <!-- <div class="row" style="width: 60%;">
+          <div class="col">
+            <p class="card-text">
+              <strong>Start Date:</strong> {{ formatDate(user.start_date) }} <br />
+            </p>    
+          </div>
+          <div class="col">
+            <p class="card-text">
+              <strong>End Date:</strong> {{ formatDate(user.end_date) }}
+            </p>    
+          </div>
+        </div> -->
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { mapGetters } from 'vuex';
+  export default {
+    name: "UserCard",
+    props: {
+      user: {
+        type: Object,
+        required: true,
+      },
+      userType: {
+        type: String, // e.g., 'admin', 'sponsor', 'influencer'
+        required: true,
+      },
+      userName: {
+        type: String,
+        required: true,
+      },
+      userID: {
+        type: String,
+        required: true,
+      },
+    },
+    computed: {
+      ...mapGetters(['isDarkTheme']),
+      isSponsor() {
+        return this.user.sponsor_id === Number(this.userID);
+      },
+      isAdmin() {
+        return this.userType === "admin";
+      },
+      isInfluencer() {
+        return this.userType === "influencer";
+      },
+    },
+    methods: {
+      formatDate(date) {
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        return new Date(date).toLocaleDateString(undefined, options);
+      },
+      
+      toggleFlag() {
+        this.$emit('toggle-flag',this.user);
+      },
+      authorize(){
+        this.$emit('authorize',this.user);
+      }
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .user-card {
+    border: 1px solid #ddd;
+    border-radius: 0.25rem;
+    width: 20rem;
+    transition: background 0.3s ease, color 0.3s ease;
+  }
+  
+  .user-card.dark {
+    background-color: #333;
+    color: #f4f4f4;
+  }
+  
+  .card-header {
+    background-color: #f8f9fa;
+  }
+  
+  .card-header.dark {
+    background-color: #444;
+  }
+  
+  .card-body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .card-body.dark {
+    background-color: #444;
+  }
+  
+  .card-footer {
+    background-color: #f8f9fa;
+  }
+  
+  .card-footer.dark {
+    background-color: #444;
+  }
+  
+  .badge {
+    font-size: 0.8rem;
+    padding: 0.5em;
+  }
+  
+  .badge.dark {
+    color: #f4f4f4;
+  }
+  
+  .btn {
+    margin-right: 0.5em;
+  }
+  
+  .card-text {
+    overflow: auto;
+    text-overflow: ellipsis;
+    /* word-break: break-word;
+    word-wrap: break-word; */
+    max-width: 60rem;
+    max-height: 6rem;
+  }
+  
+  .card-text::-webkit-scrollbar {
+    display: none;
+  }
+  
+  .card-text.dark {
+    color: #f4f4f4;
+  }
+  </style>
+  
---
+++ app/src/components/Users.vue
@@ -0,0 +1,260 @@
+<template>
+    <div :class="['user-page', { dark: isDarkTheme }]">
+      <!-- Tabs -->
+      <div class="tabs">
+        <button 
+          v-for="tab in tabs" 
+          :key="tab" 
+          @click="setActiveTab(tab)" 
+          :class="[
+            { active: activeTab === tab },
+            { dark: isDarkTheme }
+          ]"
+        >
+          {{ tab }}
+        </button>
+      </div>
+      <!-- user List -->
+      <div class="user-list">
+        <UserCard
+          v-for="user in filteredUsers" :key="user.id"
+          :user="user"
+          :userType="userType"
+          :userName="userName"
+          :userID="userID"
+          @toggle-flag="toggleFlag"
+          @authorize="authorize"
+        />
+      </div>
+      
+    </div>
+  </template>
+  
+  <script>
+  import { mapState, mapGetters } from "vuex";
+  import UserCard from "./UserCard.vue";
+  
+  export default {
+    name: "UserPage",
+    components: {
+      UserCard,
+    },
+    data() {
+      return {
+        activeTab: "All",
+        tabs:['All','Influencer','Sponsor','Unauthorized','Authorized','Flagged'],
+        users: [],
+        showAdduserForm: false,
+      };
+    },
+    computed: {
+      ...mapState(["userType", "user", "userName", "userID", "searchQuery"]),
+      ...mapGetters(["isDarkTheme","token"]),
+      filteredUsers() {
+        let filtered = this.users;
+  
+        // Filter by tab
+        if (this.activeTab === "Authorized")
+        {
+            filtered = filtered.filter((c) => c.flag === "Authorized");
+        }
+        else if (this.activeTab === "Unauthorized")
+        {
+            filtered = filtered.filter((c) => c.flag === "Unauthorized");
+        }
+        else if (this.activeTab === "Flagged")
+        {
+            filtered = filtered.filter((c) => c.flag === "Flagged");
+        }
+        else if (this.activeTab !== "All") {
+            filtered = filtered.filter((c) => c.userType === this.activeTab);
+        }        
+
+        console.log("Users: ",filtered)
+  
+        // Filter by search query
+        if (this.searchQuery) {
+          filtered = filtered.filter((c) =>
+            c.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+          );
+        }
+  
+        return filtered;
+      },
+    },
+    mounted() {
+      this.fetchUsers();
+    },
+    methods: {
+        setActiveTab(tab) {
+            this.activeTab = tab;
+        },
+        async fetchUsers() {
+            try {
+              const url = "http://127.0.0.1:5000/auth/users";
+              const headers = {
+                "Content-Type": "application/json",
+                "Authorization":this.token,
+                "search-query": this.searchQuery,
+              };
+              const response = await fetch(url, {
+                method: "GET",
+                headers: headers,
+              });
+              if (!response.ok) {
+                throw new Error(`API error: ${response.status}`);
+              }
+              const data = await response.json();
+              this.users = data;
+            } catch (error) {
+              console.error("Failed to fetch users:", error);
+            }
+        },
+        async toggleFlag(user) {
+            try {
+                const flag = user.flag === "Authorized" ? "Flagged" : "Authorized";
+
+              // Send PUT request to update the flag in the database
+                const response = await fetch('http://127.0.0.1:5000/auth/users', {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": this.token
+                    },
+                    body: JSON.stringify({ id:user.id,flag: flag }),
+                });
+
+                if (!response.ok) {
+                    const error = await response.json();
+                    console.error("Error updating flag:", error.message);
+                    return;
+                }
+
+                const data = await response.json();
+
+                // Update the local user flag
+                user.flag = data.flag;
+            }catch (error) {
+              console.error("Failed to fetch users:", error);
+            }
+        },
+        async authorize(user){
+            try{
+                const response = await fetch('http://127.0.0.1:5000/auth/users', {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": this.token
+                    },
+                    body: JSON.stringify({ id:user.id,flag:"Authorized" }),
+                });
+
+                if (!response.ok) {
+                    const error = await response.json();
+                    console.error("Error authorizing:", error.message);
+                    return;
+                }
+
+                const data = await response.json();
+
+                // Update the local user flag
+                user.flag = data.flag;
+            }catch(error) {
+              console.error("Failed to fetch users:", error);
+            }
+        }
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .add-btn {
+    position: fixed;
+    bottom: 1.5rem;
+    right: 1.5rem;
+    width: 3rem;
+    height: 3rem;
+    border: none;
+    border-radius: 50%;
+    background: #47c002;
+    color: #fff;
+    font-size: 1.5rem;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    transition: background 0.3s ease;
+  }
+  .add-btn.dark{
+    background: #4124e2;
+  }
+  .add-btn:hover {
+    background: #4ed400;
+  }
+  .add-btn.dark:hover {
+    background: #4d2ff7;
+  }
+  .user-page {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 0.25rem;
+    min-height: 86vh;
+    max-height: 86vh;
+    background: #f9f9f9;
+    color: #333;
+    transition: background 0.3s ease, color 0.3s ease;
+  }
+  
+  .user-page.dark {
+    background: #1e1e1e;
+    color: #f4f4f4;
+  }
+  
+  .tabs {
+    display: flex;
+    gap: 1rem;
+  }
+  
+  .tabs button {
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    border: none;
+    background: #eee;
+    border-radius: 5px;
+    transition: background 0.3s ease, color 0.3s ease;
+  }
+  
+  .tabs button.active {
+    background: #555;
+    color: white;
+  }
+  
+  .tabs button.dark {
+    background: #333;
+    color: #f4f4f4;
+  }
+  
+  .tabs button.active.dark {
+    background: #4124e2;
+    color: #fff;
+  }
+  
+  .user-list {
+    margin-top: 0.5rem;
+    padding: 0.25rem;
+    display: flex;
+    flex-wrap: wrap;
+    overflow-y: auto;
+    gap: 0.75rem;
+  }
+  
+  .user-item {
+    background: #f4f4f4;
+    border-radius: 5px;
+    transition: background 0.3s ease;
+  }
+  
+  .user-item.dark {
+    background: #333;
+  }
+  </style>
+  
