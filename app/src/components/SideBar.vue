a98952edbeaefe9cd7be0bc24ac7d7a66c486cea
ironman123 <rajulyadav123@gmail.com>
2024-11-26 16:10:57 +0530

    Updated Sidebar for new Pages

--- app/src/components/SideBar.vue
+++ app/src/components/SideBar.vue
@@ -28,11 +28,11 @@
         isCollapsed: false,
         items: [
           { name: 'Home', icon: '/sidebaricons/home.png', route: '/home', roles: ['admin', 'influencer', 'sponsor'] },
+          { name: 'Users', icon: '/sidebaricons/users.png', route: 'users', roles: ['admin'] },
           { name: 'Campaigns', icon: '/sidebaricons/campaigns.png', route: 'campaigns', roles: ['admin','influencer','sponsor'] },
           { name: 'Requests', icon: '/sidebaricons/requests.png', route: 'requests', roles: ['admin','influencer','sponsor'] },
           { name: 'Settings', icon: '/sidebaricons/settings.png', route: '/settings', roles: ['admin', 'influencer'] },
           { name: 'Profile', icon: '/sidebaricons/profile.png', route: '/profile', roles: ['admin', 'influencer', 'sponsor'] },
-          { name: 'Manage Users', icon: '/sidebaricons/manage-users.png', route: '/manage-users', roles: ['admin'] },
           
         ],
       }
